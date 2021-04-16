518e332f320ca8cb5bbf5b4e049971f685c789eb
KleverBarahona97 <77896505+KleverBarahona97@users.noreply.github.com>
2021-04-16 00:41:48 -0500

    ULTIMO

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,15 @@
 var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
+	overlay1 = document.getElementById('overlay1'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
 btnAbrirPopup.addEventListener('click', function(){
-	overlay.classList.add('active');
+	overlay1.classList.add('active');
 	popup.classList.add('active');
 });
 
 btnCerrarPopup.addEventListener('click', function(e){
 	e.preventDefault();
-	overlay.classList.remove('active');
+	overlay1.classList.remove('active');
 	popup.classList.remove('active');
 });
